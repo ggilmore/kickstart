e25814671553f3d407a7e6d27aa42121522ece53
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-09-04 13:44:30 -0700

    fix script

--- ci/asdf-install.sh
+++ ci/asdf-install.sh
@@ -4,8 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/..
 set -euxo pipefail
 
 DEFAULT_JUST_VERSION="${DEFAULT_JUST_VERSION:-0.7.2}"
-
-asdf global just "${DEFAULT_JUST_VERSION}"
 asdf install just "${DEFAULT_JUST_VERSION}"
+asdf global just "${DEFAULT_JUST_VERSION}"
 
 just install
