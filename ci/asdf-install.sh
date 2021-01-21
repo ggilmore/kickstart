c8009c22fa23c05a6462f8d2a2df59bcf71acc6c
Geoffrey Gilmore <tkdmaster123@gmail.com>
2021-01-21 10:14:24 -0800

    ci: remove deprecated set-env statements (#3)

--- ci/asdf-install.sh
+++ ci/asdf-install.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 set -euxo pipefail
 
-DEFAULT_JUST_VERSION="${DEFAULT_JUST_VERSION:-0.7.2}"
+DEFAULT_JUST_VERSION="${DEFAULT_JUST_VERSION:-0.8.3}"
 asdf install just "${DEFAULT_JUST_VERSION}"
 asdf global just "${DEFAULT_JUST_VERSION}"
 
