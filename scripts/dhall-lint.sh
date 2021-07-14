4a4e1c15a7f399e75d46c8e10f7d01e8666a866f
Geoffrey Gilmore <me@ggilmore.net>
2021-07-14 15:53:49 -0700

    upgrade dhall to 1.39.0

--- scripts/dhall-lint.sh
+++ scripts/dhall-lint.sh
@@ -7,30 +7,4 @@ DHALL_FILES=()
 
 mapfile -t DHALL_FILES < <(scripts/ls-dhall-files.sh)
 
-function lint() {
-  local file="$1"
-
-  local LINT_ARGS=(
-    "lint"
-    "--inplace"
-    "${file}"
-  )
-
-  if [ "${CHECK:-"false"}" == "true" ]; then
-    LINT_ARGS+=("--check")
-  fi
-
-  result=$(dhall "${LINT_ARGS[@]}" 2>&1)
-  rc=$?
-
-  if [ -n "$result" ]; then
-    echo "${file}:"
-    echo "$result"
-    echo
-  fi
-
-  exit "$rc"
-}
-export -f lint
-
-./scripts/parallel_run.sh lint {} ::: "${DHALL_FILES[@]}"
+dhall lint "${DHALL_FILES[@]}"
