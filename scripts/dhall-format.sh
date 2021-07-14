ac13418d6f17137b4e8efd63ad7324e2b2fe95a8
Geoffrey Gilmore <tkdmaster123@gmail.com>
2021-07-14 15:57:14 -0700

    upgrade dhall to 1.39.0 (#8)

--- scripts/dhall-format.sh
+++ scripts/dhall-format.sh
@@ -7,30 +7,4 @@ DHALL_FILES=()
 
 mapfile -t DHALL_FILES < <(scripts/ls-dhall-files.sh)
 
-function format() {
-  local file="$1"
-
-  local FORMAT_ARGS=(
-    "format"
-    "--inplace"
-    "${file}"
-  )
-
-  if [ "${CHECK:-"false"}" == "true" ]; then
-    FORMAT_ARGS+=("--check")
-  fi
-
-  result=$(dhall "${FORMAT_ARGS[@]}" 2>&1)
-  rc=$?
-
-  if [ -n "$result" ]; then
-    echo "${file}:"
-    echo "$result"
-    echo
-  fi
-
-  exit "$rc"
-}
-export -f format
-
-./scripts/parallel_run.sh format {} ::: "${DHALL_FILES[@]}"
+dhall format "${DHALL_FILES[@]}"
