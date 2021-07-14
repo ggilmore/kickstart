c8ab4a9851207ecd907fdfc94a98b7f7d707e6db
Geoffrey Gilmore <me@ggilmore.net>
2021-07-14 16:04:05 -0700

    dhall: restore "check" flags for ci

--- scripts/dhall-format.sh
+++ scripts/dhall-format.sh
@@ -4,7 +4,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/..
 set -eu pipefail
 
 DHALL_FILES=()
-
 mapfile -t DHALL_FILES < <(scripts/ls-dhall-files.sh)
 
-dhall format "${DHALL_FILES[@]}"
+OPTIONS=(
+  "format"
+)
+
+if [ "${CHECK:-"false"}" == "true" ]; then
+  OPTIONS+=("--check")
+fi
+
+FORMAT_ARGS=("${OPTIONS[@]}" "${DHALL_FILES[@]}")
+
+dhall "${FORMAT_ARGS[@]}"
